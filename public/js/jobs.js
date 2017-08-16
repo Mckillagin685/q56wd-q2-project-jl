72a04fe9053296235c176b6ad751badcb769cffe
Lucas Kirsch <grimreaper685@gmail.com>
2017-08-16 14:06:46 -0700

    cleaning code in public/js/jobs.js

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,156 +1,6 @@
 'use strict';
 
-// $('.parallax').parallax();
-// console.log('1');
-
-$(".logout").hide();
 $('#glassDoor').hide()
-
-$.getJSON('/user')
-  .done((user) => {
-    const $helloUser = $('.helloUser');
-    const firstName = ('hello ' + user[0].first_name);
-    // console.log('user: ', user);
-    // console.log('firstName: ',firstName);
-    if (user) {
-      console.log(firstName);
-      $helloUser.text(firstName).append;
-      $(".login").hide();
-      $(".logout").show();
-    } else {
-      $(".login").show();
-      $(".logout").hide();
-    }
-  })
-
-const $logout = $('.logout');
-
-$logout.click((event) => {
-  event.preventDefault();
-
-  const options = {
-    dataType: 'json',
-    type: 'DELETE',
-    url: '/token'
-  };
-
-  $.ajax(options)
-    .done(() => {
-      // console.log('I am Loged Out and I need to refresh this page');
-      // window.location.href = '/index.html';
-    })
-    .fail(() => {
-      // Materialize.toast('Unable to log out. Please try again.', 3000);
-      window.location.href = '/index.html';
-    });
-});
-
-// function() {
-'use strict';
-// console.log($('#loginForm'));
-$('#loginForm').submit((event) => {
-  event.preventDefault();
-  // console.log('submit has been triggered');
-
-  const email = $('#email').val().trim();
-  const password = $('#password').val();
-
-  if (!email) {
-    return Materialize.toast('Email must not be blank', 3000);
-  }
-
-  if (!password) {
-    return Materialize.toast('Password must not be blank', 3000);
-  }
-
-  const options = {
-    contentType: 'application/json',
-    data: JSON.stringify({
-      email,
-      password
-    }),
-    dataType: 'json',
-    type: 'POST',
-    url: '/token'
-  };
-
-  $.ajax(options)
-    .done((user) => {
-      // console.log(user.firstName);
-      let helloUser = ('hello ' + user.firstName);
-      $(".helloUser").text(helloUser);
-      window.location.href = '/search.html';
-    })
-    .fail(($xhr) => {
-      Materialize.toast($xhr.responseText, 3000);
-    });
-});
-
-$('#signUpForm').submit((event) => {
-  event.preventDefault();
-  // console.log('submit has been triggered');
-
-  const firstName = $('#firstName').val().trim();
-  const lastName = $('#lastName').val().trim();
-  const email = $('#emailSU').val().trim();
-  const password = $('#passwordSU').val();
-
-  if (!firstName) {
-    return Materialize.toast('First name must not be blank', 3000);
-  }
-
-  if (!lastName) {
-    return Materialize.toast('Last name must not be blank', 3000);
-  }
-
-  if (!email) {
-    return Materialize.toast('Email must not be blank', 3000);
-  }
-
-  if (email.indexOf('@') < 0) {
-    return Materialize.toast('Email must be valid', 3000);
-  }
-
-  if (!password || password.length < 8) {
-    return Materialize.toast(
-      'Password must be at least 8 characters long',
-      3000
-    );
-  }
-
-  const options = {
-    contentType: 'application/json',
-    data: JSON.stringify({
-      firstName,
-      lastName,
-      email,
-      password
-    }),
-    dataType: 'json',
-    type: 'POST',
-    url: '/users'
-  };
-
-  $.ajax(options)
-    .done(() => {
-      window.location.href = '/search.html';
-    })
-    .fail(($xhr) => {
-      Materialize.toast($xhr.responseText, 3000);
-    });
-});
-
-$(document).ready(function() {
-  // console.log($('.modal'));
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-});
-
-// $(document).ready(function(){
-$('.collapsible').collapsible();
-// });
-
-// console.log('2');
 
 // ***********************************************MAIN PAGE
 
@@ -239,8 +89,6 @@ const renderJobs = function() {
 
   let job = {}
   for (job of jobs) {
-    // console.log(job);
-    // console.log(job.title);
     let $jobTitle = $('<button>').text(job.title).attr('id', job.url)
     let $td = $('<td>')
     let $jobCompany = $('<td>').text(job.company)
@@ -255,15 +103,10 @@ const renderJobs = function() {
     $tr.append($jobLocation)
     $tr.append($jobDate)
     tBody.append($tr)
-
-    // console.log(tBody);
-
-    // window.setTimeout(continue, 200);
   }
 
   $(".getJob").click(function(event) {
     event.preventDefault();
-    // console.log('Submit was clicked');
 
     $("#job").empty();
 
@@ -272,21 +115,14 @@ const renderJobs = function() {
     $('#job').slideUp('slow')
 
     const jobUrl = event.target.id
-    // console.log(jobUrl);
     let jobTitle = ''
     let jobCompany = ''
     let jobLocation = ''
-    let jobDate = ''
-
-    // console.log(jobs.length);
 
     for (let i = 0; i < jobs.length; i++) {
-      // console.log(jobs[i]);
-      // console.log(jobs[i].id);
 
       if (jobs[i].url === jobUrl) {
         jobTitle = jobs[i].title
-        // console.log('jobTitle: ' + jobTitle);
         jobCompany = jobs[i].company
         jobLocation = jobs[i].location
         jobDate = jobs[i].date
@@ -313,7 +149,6 @@ const renderJobs = function() {
     $table.append($tbody)
     jobsTable.append($table)
 
-    // console.log(jobTitle);
 
     let $jobTitle = $('<a>').text(jobTitle).attr({
       href: job.url,
