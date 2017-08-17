789832f138895d15fcd6ce8e13468fcde6fd61cd
Jeff Mobley <njg26crux@gmail.com>
2017-08-17 13:14:22 -0700

    changed style per Scott

--- routes/jobs.js
+++ routes/jobs.js
@@ -12,7 +12,7 @@ module.exports = router;
 
 router.get('/jobs', (req, res, next) => {
   const searchString = req.body.searchString;
-
+  console.log(searchString);
   axios.get(`http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=${searchString}`)
   .then((resp) => {
     res.send(resp.data);
